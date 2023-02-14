8d73cdebfae04af39ba0c0c0ce0c4abe6c474f64
niloferabdul4 <niloferabdul4@gmail.com>
2023-02-14 13:03:18 +0400

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { useState } from 'react';
 import {GrSearch} from 'react-icons/gr'
-import { HeaderContainer,Input,Button} from '../styles'
+import { HeaderContainer,Input,Button,SearchBox,Hamburger} from '../styles'
 import { useContext } from 'react';
 import { AppContext } from '../App';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { GrClose } from 'react-icons/gr';
 
 const Header = () => {
 
      const {setUrl}=useContext(AppContext)
-
      const[searchText,setSearchText]=useState('')
+     const [menuOpen,setMenuOpen]=useState(false)
 
      const handleChange=(event)=>
      {
@@ -22,17 +24,22 @@ const Header = () => {
      }
 
      
-  
 
     return (
       <HeaderContainer>
 
-       
-             <Input type='text' placeholder='Search meals here...' onChange={handleChange} />
-             <Button name='search' onClick={searchRecipe}>
-                  <GrSearch size={26} />
-             </Button>
-                         
+             <h1><span>Recipes</span> App</h1>
+             <SearchBox>
+               <Input type='text' placeholder='Search meals here...' onChange={handleChange} />
+               <Button name='search' onClick={searchRecipe}>
+                    <GrSearch size={26} />
+               </Button>
+             </SearchBox>
+             
+             <Hamburger>                        
+                        {menuOpen? (<GrClose size={24} color={'white'} onClick={()=>setMenuOpen(false)}/>): (<GiHamburgerMenu  size={24} onClick={()=>setMenuOpen(true)}/>)}        
+                       
+            </Hamburger>            
     
        
       </HeaderContainer>
